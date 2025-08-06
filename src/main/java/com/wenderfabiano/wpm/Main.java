d3c636940ced1477985d567296a31d8c3fcc98a3
wenderf5 <wenderfabianoalves@gmail.com>
2025-08-05 21:00:10 -0300

    feat: 'get valid actions from config xml file' feature added

--- src/main/java/com/wenderfabiano/wpm/Main.java
+++ src/main/java/com/wenderfabiano/wpm/Main.java
@@ -1,9 +1,13 @@
 package com.wenderfabiano.wpm;
 
 import com.wenderfabiano.wpm.validator.Validator;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
         Validator validator = new Validator();
         validator.validate(args);
     }
--- src/main/java/com/wenderfabiano/wpm/validator/Validator.java
+++ src/main/java/com/wenderfabiano/wpm/validator/Validator.java
@@ -2,13 +2,26 @@ package com.wenderfabiano.wpm.validator;
 
 import com.wenderfabiano.wpm.exceptions.MultipleActionsException;
 import com.wenderfabiano.wpm.exceptions.UnrecognizedActionException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Validator {
-    private final List<String> actions = new ArrayList<>(Arrays.asList("run", "compile", "package"));
+    private final List<String> actions;
+
+    public Validator() throws ParserConfigurationException, IOException, SAXException {
+        this.actions = getActions();
+    }
 
     public void validate(String... actions) {
         try {
@@ -41,5 +54,26 @@ public class Validator {
                 throw new MultipleActionsException("We noticed an inconsistency in your command. The action '" + actions[i] + "' appears more than once.");
             }
         }
+    }
+
+    private List<String> getActions() throws ParserConfigurationException, IOException, SAXException {
+        List<String> validActions = new ArrayList<>();
+
+        InputStream xmlFile = Validator.class.getClassLoader().getResourceAsStream("actions-config.xml");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(xmlFile);
+
+        NodeList actionsList = doc.getElementsByTagName("action");
+
+        for (int i = 0; i < actionsList.getLength(); i++) {
+            if (actionsList.item(i).getNodeType() == Node.ELEMENT_NODE) {
+                Element element = (Element) actionsList.item(i);
+                String command = element.getElementsByTagName("command").item(0).getTextContent();
+
+                validActions.add(command);
+            }
+        }
+        return validActions;
     }
 }
